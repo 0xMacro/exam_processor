0c21d2c7280caee229480cafa156bed08e121f37
Alex Sumner <alex_sumner@mentor.com>
2021-12-13 21:29:14 +0000

    code and tests

---
+++ contracts/ExaminationResultProcessor.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.2;
+
+contract ExaminationResultProcessor {
+
+    struct ResultData {
+        uint studentId;
+        string subject;
+        uint score;
+    }
+    uint constant passMark = 60;
+
+    ResultData currentResult;
+    mapping (uint => mapping (string => bool)) public subjectsPassed;
+    mapping (uint => uint) public totalScore;
+
+    function process(ResultData[] memory results) public {
+        for (uint i=0; i<results.length; i++) {
+            currentResult = results[i];
+            processCurrentResult();
+        }
+    }
+
+    function processCurrentResult() private {
+        uint studentId = currentResult.studentId;
+        string memory subject = currentResult.subject;
+        uint score = currentResult.score;
+        subjectsPassed[studentId][subject] = score >= passMark;
+        totalScore[studentId] += score;
+    }
+
+}
