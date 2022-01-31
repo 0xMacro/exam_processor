f2d403802c0036498d39bcddfa61228b021e6782
Alex Sumner <alex.sumner@gmail.com>
2022-01-31 11:32:23 +0000

    removed hints

--- contracts/ImprovedProcessor.sol
+++ contracts/ImprovedProcessor.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.4;
 
 contract ImprovedProcessor {
 
-    // smaller ints, enum for subject?
     struct ResultData {
         uint studentId;
         string subject;
@@ -11,13 +10,10 @@ contract ImprovedProcessor {
     }
     uint constant passMark = 60;
 
-    // pass as param or inline the method
     ResultData currentResult;
-    // string key is not good
     mapping (uint => mapping (string => bool)) public subjectsPassed;
     mapping (uint => uint) public totalScore;
 
-    // unbounded array loop, calldata not memory, external not public
     function process(ResultData[] memory results) public {
         for (uint i=0; i<results.length; i++) {
             currentResult = results[i];
@@ -25,13 +21,11 @@ contract ImprovedProcessor {
         }
     }
 
-    // probably inline this, certainly pass data as params rather than using storage
     function processCurrentResult() private {
         uint studentId = currentResult.studentId;
         string memory subject = currentResult.subject;
         uint score = currentResult.score;
         subjectsPassed[studentId][subject] = score >= passMark;
-        // multiple unnecessary sotrage writes here, just work out the new score and update once
         totalScore[studentId] += score;
     }
 
