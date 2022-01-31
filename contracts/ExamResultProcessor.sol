b2d8d2bca29371e91241637ed0f1d529f0bc0007
Alex Sumner <alex.sumner@gmail.com>
2022-01-31 11:05:47 +0000

    copy of contract and bigger test

---
+++ contracts/ExamResultProcessor.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract ExamResultProcessor {
+
+    struct ResultData {
+        uint studentId;
+        string subject;
+        uint score;
+    }
+    uint constant passMark = 60;
+
+    ResultData currentResult;
+    mapping (uint => mapping (string => bool)) public subjectsPassed;
+    mapping (uint => uint) public totalScore;
+
+    function process(ResultData[] memory results) public {
+        for (uint i=0; i<results.length; i++) {
+            currentResult = results[i];
+            processCurrentResult();
+        }
+    }
+
+    function processCurrentResult() private {
+        uint studentId = currentResult.studentId;
+        string memory subject = currentResult.subject;
+        uint score = currentResult.score;
+        subjectsPassed[studentId][subject] = score >= passMark;
+        totalScore[studentId] += score;
+    }
+
+    function test() public view returns (ResultData memory) {
+        return ResultData(1, "Alex", 10);
+    }
+
+}
---
+++ test/ImprovedProcessor.js
@@ -0,0 +1,31 @@
+const { expect } = require("chai")
+
+describe("ImprovedProcessor contract", function () {
+
+  const subjects = ["English", "Physics", "Chemistry", "French", "History"]
+  const resultData = generateResultData()
+  
+  beforeEach(async function () {
+    improvedProcessorFactory = await ethers.getContractFactory("ImprovedProcessor")
+    improvedProcessor = await improvedProcessorFactory.deploy()
+    await improvedProcessor.deployed()
+  })
+
+  it("should process exam data", async function () {
+    await improvedProcessor.process(resultData)
+    await improvedProcessor.process(resultData)
+    await improvedProcessor.process(resultData)
+  })
+
+  function generateResultData() {
+    let _resultData = []
+    for (let subjectId = 0; subjectId < 5; subjectId++) {
+      for (let studentId = 1; studentId < 31; studentId++) {
+        _resultData.push({
+          studentId, subject: subjects[subjectId], score: Math.floor(Math.random() * 101) 
+        });
+      }
+    }
+    return _resultData
+  }
+})
